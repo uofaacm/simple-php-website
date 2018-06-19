75a899556087680d9499e95734b6847c550930ed
uofaacm <39601018+uofaacm@users.noreply.github.com>
2018-06-19 10:16:58 -0400

    Update home.php

--- content/home.php
+++ content/home.php
@@ -1,18 +1,55 @@
 
-Automation is the use of control systems and information technologies to reduce the need for human work in the production of goods and services. In the scope of industrialization, automation is a step beyond mechanization. Whereas mechanization provided human operators with machinery to assist them with the muscular requirements of work, automation greatly decreases the need for human sensory and mental requirements as well. Automation plays an increasingly important role in the world economy and in daily experience.
-<p>
-<p>
-Automation has had a notable impact in a wide range of industries beyond manufacturing (where it began). Once-ubiquitous telephone operators have been replaced largely by automated telephone switchboards and answering machines. Medical processes such as primary screening in electrocardiography or radiography and laboratory analysis of human genes, sera, cells, and tissues are carried out at much greater speed and accuracy by automated systems. Automated teller machines have reduced the need for bank visits to obtain cash and carry out transactions. In general, automation has been responsible for the shift in the world economy from industrial jobs to service jobs in the 20th and 21st centuries.
-<p>
-<p>
-  <a href="https://industrialautomation.co">Industrial automation</a> deals primarily with the automation of manufacturing, quality control and material handling processes. General purpose controllers for industrial processes include Programmable logic controllers and computers. One trend is increased use of Machine vision to provide automatic inspection and robot guidance functions, another is a continuing increase in the use of robots.
-<p>
-<p>
-Energy efficiency in industrial processes has become a higher priority. Semiconductor companies like Infineon Technologies are offering 8-bit micro-controller applications for example found in motor controls, general purpose pumps, fans, and ebikes to reduce energy consumption and thus increase efficiency.
-<p>
-<p>
-An industrial robot used in automation is officially defined by ISO[1] as an automatically controlled, reprogrammable, multipurpose manipulator programmable in three or more axes. The field of robotics may be more practically defined as the study, design and use of robot systems for manufacturing (a top-level definition relying on the prior definition of robot).
-<p>
-<p>
 
-Typical applications of robots include welding, painting, ironing, assembly, pick and place, packaging and palletizing, product inspection, and testing, all accomplished with high endurance, speed, and precision.
+<strong><u>Components of robots:</strong></u>
+<br>
+<strong>Structure</strong>
+The structure of a robot is usually mostly mechanical and can be called a kinematic chain (its functionality being similar to the skeleton of the human body). The chain is formed of links (its bones), actuators (its muscles), and joints which can allow one or more degrees of freedom. Most contemporary robots use open serial chains in which each link connects the one before to the one after it. These robots are called serial robots and often resemble the human arm. Some robots, such as the Stewart platform, use a closed parallel kinematical chain. Other structures, such as those that mimic the mechanical structure of humans, various animals, and insects, are comparatively rare. However, the development and use of such structures in robots is an active area of research (e.g. biomechanics). Robots used as manipulators have an end effector mounted on the last link. This end effector can be anything from a welding device to a mechanical hand used to manipulate the environment.
+<br>
+<br>
+<strong>Power source</strong>
+At present; mostly (lead-acid) batteries are used, but potential powersources could be:
+<br>
+compressed air canisters (see air car)
+flywheel energy storage
+organic garbages (trough anaerobic digestion
+feces (human, animal); may be interesting in a military context as feces of small combat groups may be reused for the energy requirements of the robot assistant (see DEKA's project Slingshot stirling engine on how the system would operate)
+still untested energy sources (eg Joe Cell, ...)
+radioactive source (such as with the proposed Ford car of the '50); too proposed in movies as Red Planet (film)
+<br>
+<br>
+<strong>Actuation</strong>
+<br>
+
+A robot leg powered by Air Muscles
+Actuators are the "muscles" of a robot, the parts which convert stored energy into movement. By far the most popular actuators are electric motors, but there are many others, powered by electricity, chemicals, and compressed air.
+<br>
+<br>
+<strong>Motors: </strong>The vast majority of <a href="https://industrialautomation.co/product-category/robotics/">Industrial robotics</a> use electric motors, including brushed and brushless DC motors.
+Stepper motors: As the name suggests, stepper motors do not spin freely like DC motors; they rotate in discrete steps, under the command of a controller. This makes them easier to control, as the controller knows exactly how far they have rotated, without having to use a sensor. Therefore, they are used on many robots and CNC machines.
+Piezo motors: A recent alternative to DC motors are piezo motors or ultrasonic motors. These work on a fundamentally different principle, whereby tiny piezoceramic elements, vibrating many thousands of times per second, cause linear or rotary motion. There are different mechanisms of operation; one type uses the vibration of the piezo elements to walk the motor in a circle or a straight line.[10] Another type uses the piezo elements to cause a nut to vibrate and drive a screw. The advantages of these motors are nanometer resolution, speed, and available force for their size. These motors are already available commercially, and being used on some robots.
+<br>
+<strong>Air muscles:</strong></strong> The air muscle is a simple yet powerful device for providing a pulling force. When inflated with compressed air, it contracts by up to 40% of its original length. The key to its behavior is the braiding visible around the outside, which forces the muscle to be either long and thin, or short and fat. Since it behaves in a very similar way to a biological muscle, it can be used to construct robots with a similar muscle/skeleton system to an animal. For example, the Shadow robot hand uses 40 air muscles to power its 24 joints.
+<strong>Electroactive polymers: </strong>Electroactive polymers are a class of plastics which change shape in response to electrical stimulation.[15] They can be designed so that they bend, stretch, or contract, but so far there are no EAPs suitable for commercial robots, as they tend to have low efficiency or are not robust.[16] Indeed, all of the entrants in a recent competition to build EAP powered arm wrestling robots, were beaten by a 17 year old girl.[17] However, they are expected to improve in the future, where they may be useful for microrobotic applications.
+</trong>Elastic nanotubes: </strong>These are a promising, early-stage experimental technology. The absence of defects in nanotubes enables these filaments to deform elastically by several percent, with energy storage levels of perhaps 10J per cu cm for metal nanotubes. Human biceps could be replaced with an 8mm diameter wire of this material. Such compact "muscle" might allow future robots to outrun and outjump humans.
+<br>
+<br>
+
+<strong>Sensing</strong>
+<br>
+<strong>Touch</strong>
+<br>
+Current robotic and prosthetic hands receive far less tactile information than the human hand. Recent research has developed a tactile sensor array that mimics the mechanical properties and touch receptors of human fingertips.[20] The sensor array is constructed as a rigid core surrounded by conductive fluid contained by an elastomeric skin. Electrodes are mounted on the surface of the rigid core and are connected to an impedance-measuring device within the core. When the artificial skin touches an object the fluid path around the electrodes is deformed, producing impedance changes that map the forces received from the object. The researchers expect that an important function of such artificial fingertips will be adjusting robotic grip on held objects.
+<br>
+<br>
+
+<strong>Manipulation</strong>
+Robots which must work in the real world require some way to manipulate objects; pick up, modify, destroy, or otherwise have an effect. Thus the 'hands' of a robot are often referred to as end effectors,[21] while the arm is referred to as a manipulator.[22] Most robot arms have replaceable effectors, each allowing them to perform some small range of tasks. Some have a fixed manipulator which cannot be replaced, while a few have one very general purpose manipulator, for example a humanoid hand.
+<br>
+<br>
+<br>
+
+<strong>Mechanical Grippers: </strong> One of the most common effectors is the gripper. In its simplest manifestation it consists of just two fingers which can open and close to pick up and let go of a range of small objects. See end effectors.
+Vacuum Grippers: Pick and place robots for electronic components and for large objects like car windscreens, will often use very simple vacuum grippers. These are very simple astrictive [23] devices, but can hold very large loads provided the prehension surface is smooth enough to ensure suction.
+<br>
+General purpose effectors: Some advanced robots are beginning to use fully humanoid hands, like the Shadow Hand and the Schunk hand.[24] These highly dexterous manipulators, with as many as 20 degrees of freedom and hundreds of tactile sensors[25]
+For the definitive guide to all forms of robot endeffectors, their design, and usage consult the book "Robot Grippers"
